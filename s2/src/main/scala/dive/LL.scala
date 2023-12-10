d7d7e31307840f4efde0b34836287c0c873cbb6d
S.H.Ayat <fahim.ayat@gmail.com>
2023-12-10 19:18:35 +0330

    session 2

--- s2/src/main/scala/dive/LL.scala
+++ s2/src/main/scala/dive/LL.scala
@@ -1,6 +1,41 @@
 package dive
 
+sealed trait LL
+
+case class Node(value: String, next: LL) extends LL
+
+case class Empty() extends LL
 
 object LL {
+
+  def filter(in: LL, p: String => Boolean): LL = in match {
+    case Node(value, next) if p(value) =>
+      Node(value, filter(next, p))
+    case Node(value, next) =>
+      filter(next, p)
+    case Empty() =>
+      Empty()
+  }
+  def exists(in: LL, p: String => Boolean): Boolean = in match {
+    case Node(value, next) =>
+      p(value) || exists(next, p)
+    case Empty() =>
+      false
+  }
+
+  def count(in: LL, p: String => Boolean): Int = in match {
+    case Node(value, next) if p(value) =>
+      1 + count(next, p)
+    case Node(value, next) =>
+      count(next, p)
+    case Empty() =>
+      0
+  }
+  def size(in: LL): Int = in match {
+    case Node(value, next) =>
+      1 + size(next)
+    case Empty() =>
+      0
+  }
 
 }
--- s2/src/test/scala/dive/LL.scala
+++ s2/src/test/scala/dive/LL.scala
@@ -3,9 +3,34 @@ package dive
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-
 class LLSpec extends AnyFlatSpec with Matchers {
-  "LL" should "do nothing yet" in {
-    1 shouldBe 1
+  val myList = Node("b4", Node("aa5", Node("aaaaa6", Empty())))
+
+  "LL" should "calculate size of a ll" in {
+    LL.size(Empty()) shouldBe 0
+    LL.size(myList) shouldBe 3
   }
+
+  it should "filter" in {
+    LL.size(LL.filter(myList, x => x.startsWith("a"))) shouldBe 2
+    LL.size(LL.filter(myList, x => x.startsWith("b"))) shouldBe 1
+    LL.size(LL.filter(myList, x => x.startsWith("c"))) shouldBe 0
+    LL.size(LL.filter(Empty(), x => x.startsWith("c"))) shouldBe 0
+  }
+
+  it should "support exists " in {
+    LL.exists(Empty(), x => x.size == 1) shouldBe false
+    LL.exists(myList, x => x.size == 2) shouldBe true
+    LL.exists(myList, x => x.size == 7) shouldBe false
+  }
+
+  it should "support count of" in {
+    LL.count(Empty(), x => true) shouldBe 0
+    LL.count(Empty(), x => false) shouldBe 0
+
+    LL.count(myList, x => x.size > 2) shouldBe 2
+    LL.count(myList, x => x.size >= 2) shouldBe 3
+    LL.count(myList, x => x.size >= 99) shouldBe 0
+  }
+
 }
