6eccffff15f6f9102d3bb7709086074d36450b20
S.H.Ayat <fahim.ayat@gmail.com>
2023-12-19 09:25:17 +0330

    session 3

--- s2/src/main/scala/dive/LL.scala
+++ s2/src/main/scala/dive/LL.scala
@@ -1,14 +1,113 @@
 package dive
 
-sealed trait LL
+import scala.collection.immutable
 
-case class Node(value: String, next: LL) extends LL
+sealed trait LL[T] {
+  def head: T
+  def tail: LL[T]
 
-case class Empty() extends LL
+  def filter(p: T => Boolean): LL[T]
+  def size: Int
+  def count(p: T => Boolean): Int
+
+  def take(c: Int): LL[T]
+  def drop(c: Int): LL[T]
+  def takeWhile(p: T => Boolean): LL[T]
+  def dropWhile(p: T => Boolean): LL[T]
+}
+
+case class Node[T](value: T, next: LL[T]) extends LL[T] {
+
+  override def take(c: Int): LL[T] = ???
+
+  override def drop(c: Int): LL[T] = ???
+
+  override def takeWhile(p: T => Boolean): LL[T] = ???
+
+  override def dropWhile(p: T => Boolean): LL[T] = ???
+
+  override def filter(p: T => Boolean): LL[T] = {
+    val other = next.filter(p)
+
+    if (p(value)) Node(value, other)
+    else other
+  }
+
+  override def head: T = value
+
+  override def tail: LL[T] = next
+
+  override def size: Int = 1 + next.size
+
+  override def count(p: T => Boolean): Int = {
+    val base = if (p(value)) 1 else 0
+
+    base + next.count(p)
+  }
+
+}
+
+case class Empty[T]() extends LL[T] {
+
+  override def take(c: Int): LL[T] = ???
+
+  override def drop(c: Int): LL[T] = ???
+
+  override def takeWhile(p: T => Boolean): LL[T] = ???
+
+  override def dropWhile(p: T => Boolean): LL[T] = ???
+
+  override def head: T = throw new Exception("")
+
+  override def tail: LL[T] = this
+
+  override def filter(p: T => Boolean): LL[T] = this
+
+  override def size: Int = 0
+
+  override def count(p: T => Boolean): Int = 0
+
+}
 
 object LL {
 
-  def filter(in: LL, p: String => Boolean): LL = in match {
+  def head[T](in: LL[T]): T = in match {
+    case Node(value, next) => value
+    case Empty() =>
+      throw new Exception("head on empty list")
+  }
+
+  def tail[T](in: LL[T]): LL[T] = in match {
+    case Node(value, next) => next
+    case Empty()           => Empty()
+  }
+
+  def last[T](in: LL[T]): T = in match {
+    case Node(value, next) =>
+      next match {
+        case Node(v, n) =>
+          last(next)
+        case Empty() =>
+          value
+      }
+
+    case Empty() =>
+      throw new Exception("last on empty list")
+  }
+
+  def init[T](in: LL[T]): LL[T] = in match {
+    case Node(value, next) =>
+      next match {
+        case Node(v, n) =>
+          Node(value, init(next))
+        case Empty() =>
+          Empty()
+      }
+    case Empty() =>
+      Empty()
+  }
+
+  def filter[T](in: LL[T], p: T => Boolean): LL[T] = in match {
     case Node(value, next) if p(value) =>
       Node(value, filter(next, p))
     case Node(value, next) =>
@@ -16,14 +115,14 @@ object LL {
     case Empty() =>
       Empty()
   }
-  def exists(in: LL, p: String => Boolean): Boolean = in match {
+  def exists[T](in: LL[T], p: T => Boolean): Boolean = in match {
     case Node(value, next) =>
       p(value) || exists(next, p)
     case Empty() =>
       false
   }
 
-  def count(in: LL, p: String => Boolean): Int = in match {
+  def count[T](in: LL[T], p: T => Boolean): Int = in match {
     case Node(value, next) if p(value) =>
       1 + count(next, p)
     case Node(value, next) =>
@@ -31,11 +130,24 @@ object LL {
     case Empty() =>
       0
   }
-  def size(in: LL): Int = in match {
+  def size[T](in: LL[T]): Int = in match {
     case Node(value, next) =>
       1 + size(next)
     case Empty() =>
       0
   }
+
+}
+
+object Tester {
+
+  val l = 1 :: 2 :: 3 :: Nil
+
+  val myList = Node("b4", Node("aa5", Node("aaaaa6", Empty())))
+  val test: List[String] = "b4" :: "aa5" :: "aaaaa6" :: Nil
+
+  test.head
+  test.tail
+  
 
 }
--- s2/src/test/scala/dive/LL.scala
+++ s2/src/test/scala/dive/LL.scala
@@ -12,25 +12,37 @@ class LLSpec extends AnyFlatSpec with Matchers {
   }
 
   it should "filter" in {
-    LL.size(LL.filter(myList, x => x.startsWith("a"))) shouldBe 2
-    LL.size(LL.filter(myList, x => x.startsWith("b"))) shouldBe 1
-    LL.size(LL.filter(myList, x => x.startsWith("c"))) shouldBe 0
-    LL.size(LL.filter(Empty(), x => x.startsWith("c"))) shouldBe 0
+    LL.size(LL.filter[String](myList, x => x.startsWith("a"))) shouldBe 2
+    LL.size(LL.filter[String](myList, x => x.startsWith("b"))) shouldBe 1
+    LL.size(LL.filter[String](myList, x => x.startsWith("c"))) shouldBe 0
+    LL.size(LL.filter[String](Empty(), x => x.startsWith("c"))) shouldBe 0
   }
 
   it should "support exists " in {
-    LL.exists(Empty(), x => x.size == 1) shouldBe false
-    LL.exists(myList, x => x.size == 2) shouldBe true
-    LL.exists(myList, x => x.size == 7) shouldBe false
+    LL.exists[String](Empty(), x => x.size == 1) shouldBe false
+    LL.exists[String](myList, x => x.size == 2) shouldBe true
+    LL.exists[String](myList, x => x.size == 7) shouldBe false
   }
 
   it should "support count of" in {
-    LL.count(Empty(), x => true) shouldBe 0
-    LL.count(Empty(), x => false) shouldBe 0
+    LL.count[String](Empty(), x => true) shouldBe 0
+    LL.count[String](Empty(), x => false) shouldBe 0
+    LL.count[String](myList, x => x.size > 2) shouldBe 2
+    LL.count[String](myList, x => x.size >= 2) shouldBe 3
+    LL.count[String](myList, x => x.size >= 99) shouldBe 0
+  }
 
-    LL.count(myList, x => x.size > 2) shouldBe 2
-    LL.count(myList, x => x.size >= 2) shouldBe 3
-    LL.count(myList, x => x.size >= 99) shouldBe 0
+  it should "init on empty list" in {
+    LL.init(Empty()) shouldBe Empty()
+  }
+
+  it should "init on single element list" in {
+    LL.init(Node("a", Empty())) shouldBe Empty()
+
+  }
+
+  it should "init on more than one element" in {
+    LL.init(myList) shouldBe Node("b4", Node("aa5", Empty()))
   }
 
 }
